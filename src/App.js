b039124c995a369298d50786cef009b2811ea2d1
Polapob <71330294+Polapob@users.noreply.github.com>
2020-11-05 18:15:06 +0700

    commit on

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React , {useState} from "react";
 import "./App.css";
 import Navbar from "./Navbar";
+import Input from "./Input";
+import Post from "./post";
 
+let id = 1
 function App() {
+  const [posts,setPosts] = useState([]);
+  function addPost(title){
+    const newPost = {id:id,title:title};
+    setPosts([newPost, ...posts]);
+    id +=1;
+  }
+  function deletePost(id){
+    const updatedPosts = posts.filter((post)=> post.id !== id);
+    setPosts(updatedPosts);
+  }
 return (<div className="App">
         <Navbar/>
-      </div>
-);}
+        <Input addPost={addPost}/>
+        {posts.map((post)=> <Post key = {post.id} 
+                             id ={post.id} 
+                             title = {post.title}
+                             deletePost = {deletePost}/>)}
+      </div>);}
   
 export default App;
